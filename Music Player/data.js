db15e0db2f292261c6fc773465efe9c36a44907b
Solomon Jude <solomonjude38@gmail.com>
2023-04-18 18:10:43 -0700

    Add files via upload
    Music Player built with HTML, CSS, JS.
    
    Still on process

---
+++ Music Player/data.js
@@ -0,0 +1,18 @@
+let songs = [
+    {
+        name: 'Graves-Into-Gardens',
+        path: 'music/Elevation-Worship.mp3',
+        artist: 'Elevation',
+        cover: ''
+    },
+    {
+        name: 'Yahweh',
+        path: 'music/Dunsin_Oyekan_-_Yahweh_Ft_Nathaniel_Bassey.mp3',
+        artist: 'Dunsin_Oyekan Ft Nathaniel Bassey'
+    },
+    {
+        name: 'Communion',
+        path: 'music/Maverick_City_Music_-_Communion_(Jesusful.com).mp3',
+        artist: 'Maverick_City'
+    }
+]
